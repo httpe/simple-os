d54d7d7332624e1d3db51c5eafba04dfec53799e
httpe <httpe@live.jp>
2021-01-14 22:14:59 -0500

    minor fix

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -14,9 +14,9 @@ void kernel_main(void) {
 
 	// Using the property of recursive page directory
     uint32_t *ptr_page_directory = (uint32_t*)0xFFFFFFFC; // 1111_1111_1111_1111_1111_1111_1111_1100b
-	printf("Page Directory Physical Address: 0x%x!\n", (*ptr_page_directory) - 3); // 3 are the flag bits
+	printf("Page Directory Physical Address: 0x%x!\n", (*ptr_page_directory) >> 12 << 12);
     uint32_t *ptr_boot_page_table = (uint32_t*)0xFFFFFC00; // 1111_1111_1111_1111_1111_1100_0000_0000b
-	printf("Boot Page Table Physical Address: 0x%x!\n", (*ptr_boot_page_table) - 3); // 3 are the flag bits
+	printf("Boot Page Table Physical Address: 0x%x!\n", (*ptr_boot_page_table) >> 12 << 12);
 
 	// Manually triggering a page fault
 	// Use volatile to avoid it get optimized away
