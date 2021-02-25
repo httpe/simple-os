9e09f1359ce9cc0e2768f4051168a047a918ea17
httpe <httpe@live.jp>
2021-02-24 22:06:31 -0500

    GDT add user space entries

--- kernel/arch/i386/gdt.asm
+++ kernel/arch/i386/gdt.asm
@@ -32,21 +32,41 @@ gdt_data:
     db 0x0
 
 ; TODO: Add TSS segment descriptor here
+gdt_user_code:
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 1111_1010b
+    db 1100_1111b
+    db 0x0
+
+gdt_user_data:
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 1111_0010b
+    db 1100_1111b
+    db 0x0
 
 gdt_end:
+
+
 
 ; GDT descriptor to be loaded by lgdt instruction
 gdt_descriptor:
     dw gdt_end - gdt_start - 1 ; size (16 bit), always one less of its true size
     dd gdt_start ; address (32 bit)
 
+
 ; define some constants for later use
 ; these are segment selector used in protected mode
 ; Ref: https://wiki.osdev.org/Selector
 ; When Requested Privilege Level is 0 and we are using GDT, they are the same as the byte offset into the GDT table
-; Since the GDT entry has a length of 8 bytes and and first two bits of selector is RPL, the third is to choose GDT (1b) or LDT (0b)
+; Since the GDT entry has a length of 8 bytes and and first two bits of selector is RPL, the third is to choose GDT (0b) or LDT (1b)
 ; and the remaining of the 16bits selector is the 0-based index into the GDT
 
 
 CODE_SEG equ 0000000000001_0_00b ; gdt_code - gdt_start is also correct
 DATA_SEG equ 0000000000010_0_00b ; gdt_data - gdt_start is also correct
+USER_CODE_SEG equ 0000000000011_0_11b; 0x1b
+USER_DATA_SEG equ 0000000000100_0_11b; 0x23
