2c957eb670e81899cc982b57a2e023f96495f9df
httpe <httpe@live.jp>
2021-03-13 17:17:18 -0500

    Fix screen scrolling

--- kernel/arch/i386/tty/tty.c
+++ kernel/arch/i386/tty/tty.c
@@ -64,8 +64,7 @@ void terminal_putchar(char c) {
         terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
         if (++terminal_column == VGA_WIDTH) {
             terminal_column = 0;
-            if (++terminal_row == VGA_HEIGHT)
-                terminal_row = 0;
+            ++terminal_row;
         }
     }
 
