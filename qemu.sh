e509d3bcd1a64f9368f6e2dffc145cb61eaebbf6
httpe <httpe@live.jp>
2021-03-14 17:52:04 -0400

    Loan fat fs image as hdb if exist

--- qemu.sh
+++ qemu.sh
@@ -9,10 +9,16 @@ else
   DEBUG_FLAG=""
 fi
 
+if [ -f testfs.fat ]; then
+  HDB="-hdb testfs.fat"
+else
+  HDB=""
+fi
+
 if grep -q Microsoft /proc/version; then
   echo "Windows Subsystem for Linux"
-  qemu-system-$(./target-triplet-to-arch.sh $HOST).exe ${DEBUG_FLAG} -hda bootable_kernel.bin -serial file:serial_port_output.txt
+  qemu-system-$(./target-triplet-to-arch.sh $HOST).exe ${DEBUG_FLAG} -hda bootable_kernel.bin ${HDB} -serial file:serial_port_output.txt
 else
   echo "Native Linux"
-  qemu-system-$(./target-triplet-to-arch.sh $HOST) ${DEBUG_FLAG} -hda bootable_kernel.bin -serial file:serial_port_output.txt
+  qemu-system-$(./target-triplet-to-arch.sh $HOST) ${DEBUG_FLAG} -hda bootable_kernel.bin ${HDB} -serial file:serial_port_output.txt
 fi
