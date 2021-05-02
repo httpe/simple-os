0cf1470a3fa3fbcade5c178c0b95c29fcc008fbe
httpe <httpe@live.jp>
2021-05-02 15:53:27 -0400

    Shell remember last command

--- applications/shell/shell.c
+++ applications/shell/shell.c
@@ -5,10 +5,6 @@
 #include <fs.h>
 #include <kernel/keyboard.h>
 
-#define FD_STDIN 0
-#define FD_STDOUT 1
-#define FD_STDERR 2
-
 #define MAX_COMMAND_LEN 255
 
 static inline _syscall4(SYS_READDIR, int, sys_readdir, const char *, path, uint, entry_offset, fs_dirent*, buf, uint, buf_size)
@@ -24,27 +20,38 @@ int main(int argc, char* argv[]) {
     char c;
     char command[MAX_COMMAND_LEN + 1] = {0};
     int n_command_read;
+    char prev_command[MAX_COMMAND_LEN + 1] = {0};
+    int prev_n_command_read;
 
     while(1) {
-        write(FD_STDOUT, "$ ", 2);
+        write(STDOUT_FILENO, "$ ", 2);
         memset(command, 0, MAX_COMMAND_LEN + 1);
         n_command_read = 0;
         while(1) {
-            while(read(FD_STDIN, &c, 1) <= 0);
-            if(c == '\n' || n_command_read >= MAX_COMMAND_LEN) {
-                write(FD_STDOUT, &c, 1);
+            while(read(STDIN_FILENO, &c, 1) <= 0);
+            if(c == '\n') {
+                write(STDOUT_FILENO, &c, 1);
                 break;
-            }
-            if(c == '\b') {
+            } else if(c == '\b') {
                 if(n_command_read > 0) {
-                    write(FD_STDOUT, &c, 1);
+                    write(STDOUT_FILENO, &c, 1);
                     n_command_read--;
                 }
-            } else {
-                write(FD_STDOUT, &c, 1);
+            } else if(c == KEY_UP) {
+                // clear command entered
+                for(int i=0; i<n_command_read;i++) {
+                    write(STDOUT_FILENO, &"\b", 1);
+                }
+                memmove(command, prev_command, MAX_COMMAND_LEN+1);
+                n_command_read = prev_n_command_read;
+                write(STDOUT_FILENO, command, n_command_read);
+            } else if(n_command_read < MAX_COMMAND_LEN) {
+                write(STDOUT_FILENO, &c, 1);
                 command[n_command_read++] = c;
             }
         }
+        memmove(prev_command, command, MAX_COMMAND_LEN + 1);
+        prev_n_command_read = n_command_read;
 
         char* part = strtok(command," ");
         if(part == NULL) {
