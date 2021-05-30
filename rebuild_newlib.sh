aa1cdfcea3672c4023116fcf85a94f7f136a4264
httpe <httpe@live.jp>
2021-05-30 02:18:48 -0400

    Add script to rebuild newlib

---
+++ rebuild_newlib.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export SIMPLE_OS_SRC=$(readlink -f $(dirname "$0"))
+export TOOL_CHAIN_BUILD_DIR=$SIMPLE_OS_SRC/build-toolchain
+export TOOL_CHAIN_ROOT=$SIMPLE_OS_SRC/toolchain
+export PATH=$TOOL_CHAIN_ROOT/usr/bin:$PATH
+
+######################################################
+### Rebuild Newlib with hosted Binutils and GCC
+######################################################
+
+cd $SIMPLE_OS_SRC
+
+# Install system headers
+./headers.sh
+
+cd $TOOL_CHAIN_BUILD_DIR/build-newlib
+make -j4 all
+
+make DESTDIR="$TOOL_CHAIN_ROOT" install
+cp -ar $TOOL_CHAIN_ROOT/usr/i686-simpleos/lib $TOOL_CHAIN_ROOT/usr/
+cp -ar $TOOL_CHAIN_ROOT/usr/i686-simpleos/include $TOOL_CHAIN_ROOT/usr/
+
