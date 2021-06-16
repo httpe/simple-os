65d9d55e1737a542c8cfa96a1b61c5bf7e36b7dd
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-06-15 22:31:10 -0400

    Add copy func

---
+++ applications/cp/cp.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#include <fcntl.h>
+
+int fileExist(const char* filename) {
+    struct stat buffer;
+    int exist = stat(filename, &buffer);
+    if (exist == 0) {
+	return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 3 || argv[1] == NULL || argv[2] == NULL) {
+	exit(1);
+    }
+    char buffer[1024];
+    int file1;
+    int file2;
+    ssize_t count;
+    file1 = open(argv[1], O_RDONLY);
+    if (file1 == -1) {
+	exit(1);
+    }
+    //TODO: consider the situation where argv[2] is a folder
+    if (fileExist(argv[2])) {
+	remove(argv[2]);
+	printf("Overwrite existing file\n");
+    }
+    file2 = open(argv[2], O_WRONLY | O_CREAT | O_EXCL | S_IRUSR | S_IWUSR, 0666);
+    if (file2 == -1) {
+	exit(1);
+    }
+    while ((count = read(file1, buffer, sizeof(buffer))) != 0) {
+	write(file2, buffer, count);
+    }
+    exit(0);
+}
