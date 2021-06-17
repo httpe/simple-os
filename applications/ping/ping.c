96a08b87855e63a84781bae908ca98f4a08bd17a
httpe <httpe@live.jp>
2021-06-16 22:09:52 -0400

    Remove ping pkt trailing null terminator

--- applications/ping/ping.c
+++ applications/ping/ping.c
@@ -35,7 +35,7 @@ int main(int argc, char* argv[]) {
 	"\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25" \
 	"\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35" \
 	"\x36\x37";
-	send_ethernet_packet(&(mac_addr) {.addr = "\x52\x54\x00\x12\x35\x02"}, ETHER_TYPE_IPv4, eth_payload, sizeof(eth_payload));
+	send_ethernet_packet(&(mac_addr) {.addr = "\x52\x54\x00\x12\x35\x02"}, ETHER_TYPE_IPv4, eth_payload, sizeof(eth_payload) - 1);
 
     exit(0);
 }
