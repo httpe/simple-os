1504b211e434d9e853d68e9038941ffd45130964
httpe <httpe@live.jp>
2021-06-18 14:15:49 -0400

    Fix syscall5/syscall6

--- kernel/include/syscall.h
+++ kernel/include/syscall.h
@@ -62,7 +62,7 @@ retval_type name(argtype1 arg1, argtype2 arg2, argtype3 arg3, argtype4 arg4) \
 retval_type name(argtype1 arg1, argtype2 arg2, argtype3 arg3, argtype4 arg4, argtype5 arg5) \
 {\
     int ret_code; \
-    asm volatile ("push %2; push %3; push %4; push %5; push %6; push $0; int $88; pop %%ebx; pop %%ebx; pop %%ebx; pop %%ebx; pop %%ebx" \
+    asm volatile ("push %2; push %3; push %4; push %5; push %6; push $0; int $88; pop %%ebx; pop %%ebx; pop %%ebx; pop %%ebx; pop %%ebx; pop %%ebx" \
     :"=a"(ret_code) \
     :"a"(syscall_num),  "m"((unsigned int) arg5), "m"((unsigned int) arg4), "r"((unsigned int) arg3), "r"((unsigned int) arg2), "r"((unsigned int) arg1) \
     :"ebx"); \
@@ -73,7 +73,7 @@ retval_type name(argtype1 arg1, argtype2 arg2, argtype3 arg3, argtype4 arg4, arg
 retval_type name(argtype1 arg1, argtype2 arg2, argtype3 arg3, argtype4 arg4, argtype5 arg5, argtype6 arg6) \
 {\
     int ret_code; \
-    asm volatile ("push %2; push %3; push %4; push %5; push %6; push %7; push $0; int $88; pop %%ebx; pop %%ebx; pop %%ebx; pop %%ebx; pop %%ebx" \
+    asm volatile ("push %2; push %3; push %4; push %5; push %6; push %7; push $0; int $88; pop %%ebx; pop %%ebx; pop %%ebx; pop %%ebx; pop %%ebx; pop %%ebx; pop %%ebx" \
     :"=a"(ret_code) \
     :"a"(syscall_num), "m"((unsigned int) arg6), "m"((unsigned int) arg5), "m"((unsigned int) arg4), "r"((unsigned int) arg3), "r"((unsigned int) arg2), "r"((unsigned int) arg1) \
     :"ebx"); \
