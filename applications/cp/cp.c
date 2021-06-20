6a58e489655bc607d73f442e270492db88b12e95
XiangyuG <xg673@nyu.edu>
2021-06-20 11:23:18 -0400

    Be able to copy file to folder

--- applications/cp/cp.c
+++ applications/cp/cp.c
@@ -38,24 +38,23 @@ int main(int argc, char* argv[]) {
     if (file1 == -1) {
 	    exit(1);
     }
-    //TODO: consider the situation where argv[2] is a folder
+    // consider the situation where argv[2] is a folder
     if (isFile(argv[2]) == 0) {
         if (argv[2][strlen(argv[2] - 1)] != '/') {
             strcat(argv[2], "/");
         }
         char *src = argv[1];
-        for (unsigned int i = strlen(argv[1]) - 1; i >= 0; i--) {
+        for (int i = (int) strlen(argv[1]) - 1; i >= 0; i--) {
             if (argv[1][i] == '/') {
                 src += (i + 1);
                 break;
             }
         }
         strcat(argv[2], src);
-    } else {
-        if (fileExist(argv[2])) {
-	        remove(argv[2]);
-	        printf("Overwrite existing file\n");
-        }
+    }
+    if (fileExist(argv[2])) {
+        remove(argv[2]);
+	    printf("Overwrite existing file\n");
     }
     file2 = open(argv[2], O_WRONLY | O_CREAT | O_EXCL | S_IRUSR | S_IWUSR, 0666);
     if (file2 == -1) {
