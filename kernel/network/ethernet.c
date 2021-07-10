bb5f975d50221007172695b5948b3b7ee89eed1f
httpe <httpe@live.jp>
2021-07-10 00:43:24 -0400

    Lock ethernet

--- kernel/network/ethernet.c
+++ kernel/network/ethernet.c
@@ -1,18 +1,22 @@
-#include <string.h>
-#include <common.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <network.h>
-#include <kernel/panic.h>
 #include <kernel/ethernet.h>
 #include <kernel/arp.h>
 #include <kernel/rtl8139.h>
 #include <kernel/arp.h>
 #include <kernel/ipv4.h>
+#include <kernel/lock.h>
+#include <kernel/panic.h>
+#include <string.h>
+#include <common.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <network.h>
 
 #define CRC_POLY    0xEDB88320
 
-static void* ethernet_transmit_buffer = NULL;
+static struct {
+    void* ethernet_transmit_buffer;
+    yield_lock lk;
+} eth;
 
 // Source: https://stackoverflow.com/questions/60684565/c-crc32-checksum-does-not-match-wireshark-on-ethernet-frame-check-sequence
 static uint32_t crc32(uint8_t *data, uint len)
@@ -41,14 +45,14 @@ static uint32_t crc32(uint8_t *data, uint len)
 
 int init_ethernet()
 {
-    ethernet_transmit_buffer = malloc(RTL8139_TRANSMIT_BUF_SIZE);
-    memset(ethernet_transmit_buffer, 0, RTL8139_TRANSMIT_BUF_SIZE);
+    eth.ethernet_transmit_buffer = malloc(RTL8139_TRANSMIT_BUF_SIZE);
+    memset(eth.ethernet_transmit_buffer, 0, RTL8139_TRANSMIT_BUF_SIZE);
     return 0;
 }
 
 int send_ethernet_packet(mac_addr dest_mac, enum ether_type type, void* buf, uint16_t len)
 {
-    PANIC_ASSERT(ethernet_transmit_buffer != NULL);
+    PANIC_ASSERT(eth.ethernet_transmit_buffer != NULL);
 
     if(buf == NULL || len == 0) {
         return 0;
@@ -56,13 +60,15 @@ int send_ethernet_packet(mac_addr dest_mac, enum ether_type type, void* buf, uin
 
     uint pkt_len = sizeof(eth_header) + len;
     PANIC_ASSERT(pkt_len <= RTL8139_TRANSMIT_BUF_SIZE);
-    eth_header* header = (eth_header*) ethernet_transmit_buffer;
+    acquire(&eth.lk);
+    eth_header* header = (eth_header*) eth.ethernet_transmit_buffer;
     header->dest = dest_mac;
     header->src = rtl8139_mac();
     header->ethertype = switch_endian16(type);
     // TODO: very inefficient, should implement a buffer queue 
-    memmove(ethernet_transmit_buffer + sizeof(eth_header), buf, len);
-    int res = rtl8139_send_packet(ethernet_transmit_buffer, pkt_len);
+    memmove(eth.ethernet_transmit_buffer + sizeof(eth_header), buf, len);
+    int res = rtl8139_send_packet(eth.ethernet_transmit_buffer, pkt_len);
+    release(&eth.lk);
     return res;
 }
 
