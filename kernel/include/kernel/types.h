80a8c9dcb84b6544fa7883a631de2baa991e1555
httpe <httpe@live.jp>
2022-03-20 15:47:17 -0400

    Fix newlib/kernel type def conflict

--- kernel/include/kernel/types.h
+++ kernel/include/kernel/types.h
@@ -3,11 +3,15 @@
 
 #include <stdint.h>
 
+#ifndef _SSIZE_T_DECLARED
 typedef int32_t ssize_t;
 #define	_SSIZE_T_DECLARED
+#endif
 
+#ifndef _SUSECONDS_T_DECLARED
 typedef int32_t suseconds_t;
 #define	_SUSECONDS_T_DECLARED
+#endif
 
 typedef __SIZE_TYPE__ size_t;
 typedef int64_t time_t;
