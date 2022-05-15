23eac0d26f047f96381b92b9933d3f5f7e77f166
httpe <httpe@live.jp>
2022-05-14 22:13:28 -0400

    Protect against type re-declaration from SmallerC

--- kernel/include/kernel/types.h
+++ kernel/include/kernel/types.h
@@ -3,9 +3,14 @@
 
 #include <stdint.h>
 
+// for newlib
 #ifndef _SSIZE_T_DECLARED
+// for SmallerC libc
+#ifndef __SSIZE_T_DEF 
 typedef int32_t ssize_t;
 #define	_SSIZE_T_DECLARED
+#define	__SSIZE_T_DEF
+#endif
 #endif
 
 #ifndef _SUSECONDS_T_DECLARED
@@ -14,13 +19,19 @@ typedef int32_t suseconds_t;
 #endif
 
 #ifndef _SIZE_T_DECLARED
+#ifndef __SIZE_T_DEF
 typedef __SIZE_TYPE__ size_t;
 #define	_SIZE_T_DECLARED
+#define __SIZE_T_DEF
+#endif
 #endif
 
 #ifndef _TIME_T_DECLARED
-typedef int64_t time_t;
+#ifndef __TIME_T_DEF
+typedef int32_t time_t;
 #define	_TIME_T_DECLARED
+#define __TIME_T_DEF
+#endif
 #endif
 
 
