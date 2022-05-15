5535254b0ce117ab027b58df1cb948c5e2097c77
httpe <httpe@live.jp>
2022-05-14 22:16:12 -0400

    Support SmallerC libc (test program added)

--- applications/SmallerC/test_SmallerC.c
+++ applications/SmallerC/test_SmallerC.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+extern char** __environ;
 
 int main(int argc, char** argv)
 {
-  printf("Hello, World! (From SmallerC): argc: %d, argv[0]: %s\n", argc, argv[0]);
+  printf("Hello, World from SmallerC!\n");
+  printf("argc: %d, argv[0]: %s\n", argc, argv[0]);
+  printf("envp[\"SHELL\"]: %s\n", getenv("SHELL"));
+  time_t rawtime;
+  struct tm *info;
+  time( &rawtime );
+  info = localtime( &rawtime );
+  printf("Current UTC time and date: %s\n", asctime(info));
   return 0;
 }
--- kernel/include/fs.h
+++ kernel/include/fs.h
@@ -1,6 +1,8 @@
 #ifndef _FS_H
 #define _FS_H
 
+#include <stdint.h>
+
 // In line with newlib's unistd.h
 #define SEEK_WHENCE_SET 0
 #define SEEK_WHENCE_CUR 1
@@ -8,7 +10,7 @@
 
 #define FS_MAX_FILENAME_LEN 260
 typedef struct fs_dirent {
-    uint64_t inum; 
+    uint32_t inum; 
     char name[FS_MAX_FILENAME_LEN]; // file name, max len = 260 + null terminator (referencing FAT32 max file name)
 } fs_dirent;
 
